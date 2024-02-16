ed4bda097109466f1a266adaa5219508c84b40e4
Sanket Parab <123058949+Sanket-825@users.noreply.github.com>
2024-02-16 11:55:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+}
